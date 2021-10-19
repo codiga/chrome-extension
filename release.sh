8ff90798e3ade68bb7112b55510be4f69fcab526
Julien Delange <julien@gunnm.org>
2021-10-19 09:48:31 -0600

    update release script

--- release.sh
+++ release.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-
+find . -name '*~' | xargs rm -f
+rm -rf node_modules
 rm -rf build
 mkdir build
-zip -r build/extension.zip background.js\
-                     components \
-                     content-script.js \
-                     external \
-                     icons \
+rm -rf dist
+mkdir dist
+npm i
+npm run build
+(cd dist && zip -r ../build/extension.zip \
+                     *.js\
+                     *.ttf \
+                     *.css \
+                     *.png \
+                     *.html \
+                     js \
                      manifest.json \
-                     scripts
+                     scripts)
