2ffc6d853bf0416a9d6e06f9245ccacf4e57ac46
Daniel <daniel@code-inspector.com>
2022-08-16 10:24:47 -0700

    feat: add Codiga logo svg

---
+++ src/assets/CodigaIcon.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function CodigaIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.2087 18L22.3306 12V18L12.2087 24L2.08691 18V6L12.2087 0L22.3306 6L12.2087 12L7.14785 9.00004V15L12.2087 18Z"
+        fill="url(#paint0_linear_1312_15167)"
+      />
+      <path
+        opacity="0.6"
+        d="M17.2698 8.99996L12.2088 12L7.14795 8.99996L12.2088 6L17.2698 8.99996Z"
+        fill="white"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1312_15167"
+          x1="4.22591"
+          y1="20.1962"
+          x2="20.6125"
+          y2="4.23602"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#F71C9D" />
+          <stop offset="1" stop-color="#FC8926" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
